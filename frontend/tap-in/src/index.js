5de43e67d4c955daeb726623d3db2c3f52b7fdc8
Mathias <mathiasanderson88@gmail.com>
2020-11-19 15:55:20 -0800

    Complete setting sessions up

--- frontend/tap-in/src/index.js
+++ frontend/tap-in/src/index.js
@@ -6,12 +6,15 @@ import './index.css';
 import App from './App';
 import configureStore from '../src/store/index';
 import { restoreCSRF, fetch } from './store/csrf';
+import * as sessionActions from './store/session';
+
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
   window.csrfFetch = fetch;
   window.store = store;
+  window.sessionActions = sessionActions
   restoreCSRF()
 }
 
--- frontend/tap-in/src/store/index.js
+++ frontend/tap-in/src/store/index.js
@@ -1,11 +1,14 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
+import sessionReducer from './session';
 // import app from '../../../../backend/app';
 
 
 
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+    session: sessionReducer,
+});
 
 let enhancer;
 
